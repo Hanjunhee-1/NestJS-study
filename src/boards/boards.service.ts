029d7dcbd67c897e8af7c52f82148caa786eaf20
Hanjunhee-1 <hanjuni857@naver.com>
2023-01-02 22:09:48 +0900

    Board 모델 정의에 따른 기존 함수 및 변수의 타입 지정

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { Board } from './board.model';
 
 @Injectable()
 export class BoardsService {
   // DB 연동 대신 local 에 저장
-  private boards = [];
+  private boards: Board[] = [];
 
-  getAllBoard() {
+  getAllBoard(): Board[] {
     return this.boards;
   }
 }

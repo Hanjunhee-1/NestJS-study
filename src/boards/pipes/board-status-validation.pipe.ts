c2d1d173c09699beefc47603068e8fe4a3f7937e
Hanjunhee-1 <hanjuni857@naver.com>
2023-01-06 20:52:17 +0900

    커스텀 파이프를 통해 게시물 상태 업데이트 예외처리 로직 추가

---
+++ src/boards/pipes/board-status-validation.pipe.ts
@@ -0,0 +1,27 @@
+import {
+  PipeTransform,
+  ArgumentMetadata,
+  BadRequestException,
+} from '@nestjs/common';
+import { BoardStatus } from '../board.model';
+
+export class BaordStatusValidationPipe implements PipeTransform {
+  readonly StatusOptions = [BoardStatus.PRIVATE, BoardStatus.PUBLIC];
+
+  private isStatusValue(status: any) {
+    const index = this.StatusOptions.indexOf(status);
+    return index !== -1;
+  }
+
+  transform(value: any, metadata: ArgumentMetadata) {
+    value = value.toUpperCase();
+
+    if (!this.isStatusValue(value)) {
+      throw new BadRequestException(
+        `${BoardStatus.PRIVATE} 또는 ${BoardStatus.PUBLIC} 으로만 변경가능합니다.`,
+      );
+    }
+
+    return value;
+  }
+}

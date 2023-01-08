1635ee298b60ea97ae807144c4d33d9a5caf45c4
Hanjunhee-1 <hanjuni857@naver.com>
2023-01-09 02:31:22 +0900

    BoardStatusValidationPipe 내용 및 import 수정

--- src/boards/pipes/board-status-validation.pipe.ts
+++ src/boards/pipes/board-status-validation.pipe.ts
@@ -1,27 +1,24 @@
-// import {
-//   PipeTransform,
-//   ArgumentMetadata,
-//   BadRequestException,
-// } from '@nestjs/common';
-// import { BoardStatus } from '../board.model';
+/* eslint-disable prettier/prettier */
+import { PipeTransform, BadRequestException } from '@nestjs/common';
+import { BoardStatus } from '@prisma/client';
 
-// export class BaordStatusValidationPipe implements PipeTransform {
-//   readonly StatusOptions = [BoardStatus.PRIVATE, BoardStatus.PUBLIC];
+export class BoardStatusValidationPipe implements PipeTransform {
+  readonly StatusOptions = [BoardStatus.PRIVATE, BoardStatus.PUBLIC];
 
-//   private isStatusValue(status: any) {
-//     const index = this.StatusOptions.indexOf(status);
-//     return index !== -1;
-//   }
+  private isStatusValue(status: any) {
+    const index = this.StatusOptions.indexOf(status);
+    return index !== -1;
+  }
 
-//   transform(value: any, metadata: ArgumentMetadata) {
-//     value = value.toUpperCase();
+  transform(value: any) {
+    value = value.toUpperCase();
 
-//     if (!this.isStatusValue(value)) {
-//       throw new BadRequestException(
-//         `${BoardStatus.PRIVATE} 또는 ${BoardStatus.PUBLIC} 으로만 변경가능합니다.`,
-//       );
-//     }
+    if (!this.isStatusValue(value)) {
+      throw new BadRequestException(
+        `${this.StatusOptions.at(0)} 또는 ${this.StatusOptions.at(1)} 으로만 변경가능합니다.`,
+      );
+    }
 
-//     return value;
-//   }
-// }
+    return value;
+  }
+}

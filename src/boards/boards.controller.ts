5c03b6b6faa0c8e66ae50cfd12d6f2e075e0509c
Hanjunhee-1 <hanjuni857@naver.com>
2023-01-08 22:27:34 +0900

    DB 를 통한 CRUD 작업을 위해 기존 코드 수정

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,18 +1,5 @@
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Param,
-  Patch,
-  Post,
-  UsePipes,
-  ValidationPipe,
-} from '@nestjs/common';
-import { Board, BoardStatus } from './board.model';
+import { Controller, Get } from '@nestjs/common';
 import { BoardsService } from './boards.service';
-import { CreateBoardDto } from './dtos/create-board.dto';
-import { BaordStatusValidationPipe } from './pipes/board-status-validation.pipe';
 
 @Controller('boards')
 export class BoardsController {
@@ -25,32 +12,33 @@ export class BoardsController {
   }
   // TEST 용
 
-  @Get()
-  getAllBoard(): Board[] {
-    return this.boardsService.getAllBoard();
-  }
+  // 아래 부분은 더 이상 사용하지 않음
+  // @Get()
+  // getAllBoard(): Board[] {
+  //   return this.boardsService.getAllBoard();
+  // }
 
-  @Post()
-  @UsePipes(ValidationPipe)
-  createBoard(@Body() createBoardDto: CreateBoardDto): Board {
-    return this.boardsService.createBoard(createBoardDto);
-  }
+  // @Post()
+  // @UsePipes(ValidationPipe)
+  // createBoard(@Body() createBoardDto: CreateBoardDto): Board {
+  //   return this.boardsService.createBoard(createBoardDto);
+  // }
 
-  @Get(':id')
-  getBoardById(@Param('id') id: string): Board {
-    return this.boardsService.getBoardById(id);
-  }
+  // @Get(':id')
+  // getBoardById(@Param('id') id: string): Board {
+  //   return this.boardsService.getBoardById(id);
+  // }
 
-  @Delete(':id')
-  deleteBoardById(@Param('id') id: string): void {
-    return this.boardsService.deleteBoardById(id);
-  }
+  // @Delete(':id')
+  // deleteBoardById(@Param('id') id: string): void {
+  //   return this.boardsService.deleteBoardById(id);
+  // }
 
-  @Patch(':id')
-  updateBoardStatus(
-    @Param('id') id: string,
-    @Body('status', BaordStatusValidationPipe) status: BoardStatus,
-  ) {
-    return this.boardsService.updateBoardStatus(id, status);
-  }
+  // @Patch(':id')
+  // updateBoardStatus(
+  //   @Param('id') id: string,
+  //   @Body('status', BaordStatusValidationPipe) status: BoardStatus,
+  // ) {
+  //   return this.boardsService.updateBoardStatus(id, status);
+  // }
 }
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,13 +1,10 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
-import { Board, BoardStatus } from './board.model';
-import { v1 as uuid } from 'uuid';
-import { CreateBoardDto } from './dtos/create-board.dto';
+import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class BoardsService {
-  // DB 연동 대신 local 에 저장
-  private boards: Board[] = [];
+  // DB 연동 대신 local 에 저장 (더 이상 사용하지 않음)
+  // private boards: Board[] = [];
 
   // TEST 용
   constructor(private readonly prisma: PrismaService) {}
@@ -21,43 +18,43 @@ export class BoardsService {
   }
   // TEST 용
 
-  getAllBoard(): Board[] {
-    return this.boards;
-  }
+  // getAllBoard(): Board[] {
+  //   return this.boards;
+  // }
 
-  createBoard(createBoardDto: CreateBoardDto): Board {
-    const { title, description } = createBoardDto;
-    const board: Board = {
-      id: uuid(),
-      title,
-      description,
-      status: BoardStatus.PUBLIC,
-    };
+  // createBoard(createBoardDto: CreateBoardDto): Board {
+  //   const { title, description } = createBoardDto;
+  //   const board: Board = {
+  //     id: uuid(),
+  //     title,
+  //     description,
+  //     status: BoardStatus.PUBLIC,
+  //   };
 
-    this.boards.push(board);
-    return board;
-  }
+  //   this.boards.push(board);
+  //   return board;
+  // }
 
-  getBoardById(id: string): Board {
-    const board = this.boards.find((board) => board.id === id);
+  // getBoardById(id: string): Board {
+  //   const board = this.boards.find((board) => board.id === id);
 
-    if (!board) {
-      throw new NotFoundException(`${id} 를 가진 게시물을 찾을 수 없습니다 X(`);
-    }
+  //   if (!board) {
+  //     throw new NotFoundException(`${id} 를 가진 게시물을 찾을 수 없습니다 X(`);
+  //   }
 
-    return board;
-  }
+  //   return board;
+  // }
 
-  deleteBoardById(id: string): void {
-    const board = this.getBoardById(id);
-    if (board) {
-      this.boards = this.boards.filter((board) => board.id !== id);
-    }
-  }
+  // deleteBoardById(id: string): void {
+  //   const board = this.getBoardById(id);
+  //   if (board) {
+  //     this.boards = this.boards.filter((board) => board.id !== id);
+  //   }
+  // }
 
-  updateBoardStatus(id: string, status: BoardStatus): Board {
-    const board = this.getBoardById(id);
-    board.status = status;
-    return board;
-  }
+  // updateBoardStatus(id: string, status: BoardStatus): Board {
+  //   const board = this.getBoardById(id);
+  //   board.status = status;
+  //   return board;
+  // }
 }

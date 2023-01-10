8c2c9b667b135420101e335b29c61a0fa6ee0a9b
Hanjunhee-1 <hanjuni857@naver.com>
2023-01-11 00:16:47 +0900

    이미 존재하는 name 값으로 회원가입하려는 경우에 대한 예외처리 로직 추가

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateUserDto } from './dtos/create-user.dto';
 
@@ -9,13 +13,21 @@ export class AuthService {
   async createUser(createUserDto: CreateUserDto) {
     const { name, password } = createUserDto;
 
-    const user = await this.prisma.user.create({
-      data: {
-        name,
-        password,
-      },
-    });
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          name,
+          password,
+        },
+      });
 
-    return user;
+      return user;
+    } catch (error) {
+      if (error.meta.target === 'User_name_key') {
+        throw new ConflictException('해당 name 은 사용할 수 없습니다.');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }

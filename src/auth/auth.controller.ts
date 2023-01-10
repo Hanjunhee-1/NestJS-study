89524748e3cef1f476548c22302d78dfde419a02
Hanjunhee-1 <hanjuni857@naver.com>
2023-01-10 23:09:05 +0900

    회원가입 기능 추가 및 회원가입 시 사용될 dto 생성

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { CreateUserDto } from './dtos/create-user.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('/signUp')
+  async createUser(@Body() createUserDto: CreateUserDto) {
+    const user = await this.authService.createUser(createUserDto);
+    return user;
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateUserDto } from './dtos/create-user.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async createUser(createUserDto: CreateUserDto) {
+    const { name, password } = createUserDto;
+
+    const user = await this.prisma.user.create({
+      data: {
+        name,
+        password,
+      },
+    });
+
+    return user;
+  }
+}

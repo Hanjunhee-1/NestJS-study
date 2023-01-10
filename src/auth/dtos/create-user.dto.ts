071d1cb8c9162739d0a39901e7c49e422b3ba0e6
Hanjunhee-1 <hanjuni857@naver.com>
2023-01-10 23:31:46 +0900

    CreateUserDto 에 유효성 검사 로직 추가

--- src/auth/dtos/create-user.dto.ts
+++ src/auth/dtos/create-user.dto.ts
@@ -1,9 +1,24 @@
-import { IsNotEmpty } from 'class-validator';
+import {
+  IsNotEmpty,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class CreateUserDto {
   @IsNotEmpty()
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
   name: string;
 
   @IsNotEmpty()
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
+  @Matches(/^[a-zA-Z0-9]*$/, {
+    message: 'password must be in English and number',
+  })
   password: string;
 }

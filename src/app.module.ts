aebda4164454323263dafc78822e08f03d8316d5
Hanjunhee-1 <hanjuni857@naver.com>
2023-01-10 22:36:40 +0900

    인증기능구현을 위한 auth 모듈 및 관련 service, controller 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { BoardsModule } from './boards/boards.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [BoardsModule, PrismaModule],
-  controllers: [],
-  providers: [],
+  imports: [BoardsModule, PrismaModule, AuthModule],
 })
 export class AppModule {}

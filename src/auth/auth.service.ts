9ada8ad6c1444865f0eb67d9ff9bb8cc341ef280
Hanjunhee-1 <hanjuni857@naver.com>
2023-01-12 02:02:12 +0900

    회원가입 시 비밀번호 암호화 적용

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -5,6 +5,7 @@ import {
 } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateUserDto } from './dtos/create-user.dto';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class AuthService {
@@ -13,11 +14,16 @@ export class AuthService {
   async createUser(createUserDto: CreateUserDto) {
     const { name, password } = createUserDto;
 
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    console.log({ name, password, hashedPassword });
+
     try {
       const user = await this.prisma.user.create({
         data: {
           name,
-          password,
+          password: hashedPassword,
         },
       });
 

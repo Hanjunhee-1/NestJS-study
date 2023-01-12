ab013f2b0e542cf7fa05533c92686e8e022d8c7f
Hanjunhee-1 <hanjuni857@naver.com>
2023-01-12 23:44:16 +0900

    로그인 기능 구현

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,22 +1,29 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { CreateUserDto } from './dtos/create-user.dto';
+import { SignInDto } from './dtos/sign-in.dto';
+import { SignUpDto } from './dtos/sign-up.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('/signUp')
-  async createUser(@Body() createUserDto: CreateUserDto) {
-    const user = await this.authService.createUser(createUserDto);
+  async signUp(@Body() signUpDto: SignUpDto) {
+    const user = await this.authService.signUp(signUpDto);
     return user;
   }
 
-  // bcrypt 복호화 예시
-  @Get('/logIn')
-  async logIn(@Body('name') name: string, @Body('password') password: string) {
-    const result = await this.authService.logIn(name, password);
+  @Post('/signIn')
+  async signIn(@Body() signInDto: SignInDto) {
+    const result = await this.authService.signIn(signInDto);
     return result;
   }
+
+  // bcrypt 복호화 예시
+  // @Get('/logIn')
+  // async logIn(@Body('name') name: string, @Body('password') password: string) {
+  //   const result = await this.authService.logIn(name, password);
+  //   return result;
+  // }
   // bcrypt 복호화 예시
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,35 +2,19 @@ import {
   ConflictException,
   Injectable,
   InternalServerErrorException,
+  UnauthorizedException,
 } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { CreateUserDto } from './dtos/create-user.dto';
+import { SignUpDto } from './dtos/sign-up.dto';
 import * as bcrypt from 'bcryptjs';
+import { SignInDto } from './dtos/sign-in.dto';
 
 @Injectable()
 export class AuthService {
   constructor(private readonly prisma: PrismaService) {}
 
-  // bcrypt 복호화 예시
-  async logIn(name: string, password: string) {
-    const user = await this.prisma.user.findUnique({
-      where: {
-        name,
-      },
-    });
-
-    const isRight = await bcrypt.compare(password, user.password);
-
-    if (isRight) {
-      return user;
-    } else {
-      return '비밀번호가 잘못되었습니다.';
-    }
-  }
-  // bcrypt 복호화 예시
-
-  async createUser(createUserDto: CreateUserDto) {
-    const { name, password } = createUserDto;
+  async signUp(signUpDto: SignUpDto) {
+    const { name, password } = signUpDto;
 
     const salt = await bcrypt.genSalt();
     const hashedPassword = await bcrypt.hash(password, salt);
@@ -54,4 +38,38 @@ export class AuthService {
       }
     }
   }
+
+  async signIn(signInDto: SignInDto) {
+    const { name, password } = signInDto;
+
+    const user = await this.prisma.user.findUnique({
+      where: {
+        name,
+      },
+    });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      return 'success';
+    } else {
+      throw new UnauthorizedException('failed');
+    }
+  }
+
+  // bcrypt 복호화 예시
+  // async logIn(name: string, password: string) {
+  //   const user = await this.prisma.user.findUnique({
+  //     where: {
+  //       name,
+  //     },
+  //   });
+
+  //   const isRight = await bcrypt.compare(password, user.password);
+
+  //   if (isRight) {
+  //     return user;
+  //   } else {
+  //     return '비밀번호가 잘못되었습니다.';
+  //   }
+  // }
+  // bcrypt 복호화 예시
 }

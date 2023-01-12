ebc8e2b018377aa655ecea3d1fead847adf91f07
Hanjunhee-1 <hanjuni857@naver.com>
2023-01-13 02:11:31 +0900

    passport 및 jwt 에 관한 모듈 설정 적용

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: process.env.SECRET_KEY,
+      signOptions: {
+        expiresIn: 60 * 60,
+      },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })

c2f5d35833e161bd230e7292ad5eba9303337420
Hanjunhee-1 <hanjuni857@naver.com>
2023-01-15 02:19:37 +0900

    PrismaModule 을 전역 레벨로 설정

--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from 'src/auth/auth.module';
-import { PrismaModule } from 'src/prisma/prisma.module';
 import { BoardsController } from './boards.controller';
 import { BoardsService } from './boards.service';
 
 @Module({
   // Prisma 를 통한 CRUD 작업을 위해 꼭 필요함.
   // JWT 토큰 기반 인증을 위해 AuthModule 추가
-  imports: [PrismaModule, AuthModule],
+  imports: [AuthModule],
   controllers: [BoardsController],
   providers: [BoardsService],
 })
--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -1,6 +1,9 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 
+// 일일이 모듈마다 imports 에 PrismaModule 을 안 적어도 되게 @Global() 을 이용하여
+// 해당 모듈을 전역 레벨로 설정
+@Global()
 @Module({
   providers: [PrismaService],
   exports: [PrismaService],

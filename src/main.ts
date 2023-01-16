78e85e5ba4a1953064c27242698515f74df17323
Hanjunhee-1 <hanjuni857@naver.com>
2023-01-17 01:15:22 +0900

    logger 적용

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PrismaService } from './prisma/prisma.service';
 
 async function bootstrap() {
+  const logger = new Logger('Main');
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   const prismaService = app.get(PrismaService);
   await prismaService.enableShutDownHooks(app);
   await app.listen(3000);
+  logger.log(`Server is running at http://localhost:3000`);
 }
 bootstrap();

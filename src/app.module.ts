0d0c7544836fa4434b58559353b1dfd0ff91f74d
Hanjunhee-1 <hanjuni857@naver.com>
2023-05-02 13:47:53 +0900

    schedule module import

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,15 @@ import { BoardsModule } from './boards/boards.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { TasksModule } from './tasks/tasks.module';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
-  imports: [BoardsModule, PrismaModule, AuthModule, TasksModule],
+  imports: [
+    BoardsModule,
+    PrismaModule,
+    AuthModule,
+    TasksModule,
+    ScheduleModule.forRoot(),
+  ],
 })
 export class AppModule {}

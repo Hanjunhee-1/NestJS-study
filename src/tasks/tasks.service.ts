9a1157c8511d490dfb475b26623ebb9ab2e7a179
Hanjunhee-1 <hanjuni857@naver.com>
2023-05-02 14:46:18 +0900

    Cron 을 통해 스케줄러 구현

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,43 @@
 import { Injectable } from '@nestjs/common';
+import { Cron, CronExpression } from '@nestjs/schedule';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { getKST } from './utils/date.util';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  // 스케줄러 사용을 위해 Cron 을 사용할 것입니다.
+  // 매 10초마다 실행합니다.
+  @Cron(CronExpression.EVERY_10_SECONDS)
+  async scheduleJob() {
+    const now = getKST();
+
+    const currentPhase = await this.prisma.phase.findFirst({
+      where: {
+        start: {
+          lt: now,
+        },
+        end: {
+          gte: now,
+        },
+      },
+    });
+
+    if (currentPhase) {
+      await this.prisma.user.updateMany({
+        where: {
+          phaseId: currentPhase.id,
+        },
+        data: {
+          testCount: {
+            increment: 2,
+          },
+        },
+      });
+
+      const users = await this.prisma.user.findMany();
+      console.log(users);
+    }
+  }
+}
